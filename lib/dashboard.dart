570a9da131951a13d9cc047df9bc5ce3d43a6ed9
Abhishek Uparkar <abhishekuparkar@Abhisheks-MacBook-Air.local>
2025-11-22 14:58:37 +0530

    first commit

---
+++ lib/dashboard.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      floatingActionButton: FloatingActionButton(onPressed: (){
+        count++;
+        setState(() {
+          
+        });
+      }),
+appBar: AppBar(
+  //backgroundColor: Color.fromARGB(0, 61, 71, 213),
+  title: Text("My First App"),
+  backgroundColor: const Color.fromARGB(255, 82, 126, 140)
+),
+
+body: Center(child: Text("You have pressed this button\n$count")),
+
+    
+  );}
+}
